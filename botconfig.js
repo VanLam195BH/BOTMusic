1cbeb8e28c8e1a06b4e06d5af580b09c7b6198ba
VanLam195BH <92321760+VanLam195BH@users.noreply.github.com>
2022-04-23 21:43:33 +0700

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -31,9 +31,9 @@ module.exports = {
   // The default one should work fine, altho if you have issue with it, you can create your own or use another public lavalink.
   Lavalink: {
     id: "Main", //- Used for indentifier. You can set this to whatever you want.
-    host: "uk1.buckytm.com", //- The host name or IP of the lavalink server.
-    port: 8028112, // The port that lavalink is listening to. This must be a number!
-    pass: "f82263yh75f6r5", //- The password of the lavalink server.
+    host: "https://lavalink-repl.ahmasa.repl.co", //- The host name or IP of the lavalink server.
+    port: 2333, // The port that lavalink is listening to. This must be a number!
+    pass: "anhlam1995", //- The password of the lavalink server.
     secure: false, // Set this to true if the lavalink uses SSL. if not set it to false.
   },
 
